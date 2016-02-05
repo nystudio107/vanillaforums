10f48aa8c0177154afc2eaf5833c2841ea52b69c
Andrew Welch <andrew@megalomaniac.com>
2016-02-04 21:29:21 -0500

    Whitespace & tabs

--- services/Vanillaforums_UtilsService.php
+++ services/Vanillaforums_UtilsService.php
@@ -7,73 +7,73 @@ class Vanillaforums_UtilsService extends BaseApplicationComponent
 {
 
 /* --------------------------------------------------------------------------------
-	Output the Vanillaforums SSO jsonp
+    Output the Vanillaforums SSO jsonp
 -------------------------------------------------------------------------------- */
 
     public function outputSSO($userId = 0)
     {
-		$result = "";
-		$settings = craft()->plugins->getPlugin('vanillaforums')->getSettings();
-		$data = array();
-		
-		$currentUser = craft()->userSession->user;
-		if ($currentUser)
-		{
-			$data['uniqueid'] = $currentUser->id;
-			
-			if (craft()->config->get('useEmailAsUsername'))
-				$data['name'] = $currentUser->getFullName();
-			else
-				$data['name'] = $currentUser->username;
-			$data['email'] = $currentUser->email;
-			if ($currentUser->getPhotoUrl())
-				$data['photourl'] = $currentUser->getPhotoUrl();
-		}
-		
-		$vanillaforumsClientID = $settings['vanillaforumsClientID'];
-		$vanillaforumsSecret = $settings['vanillaforumsSecret'];
+        $result = "";
+        $settings = craft()->plugins->getPlugin('vanillaforums')->getSettings();
+        $data = array();
 
-		$secure = true;
-		
-		//ob_start(); // Start output buffering
-		
-		\WriteJsConnect($data, $_GET, $vanillaforumsClientID, $vanillaforumsSecret, $secure);
+        $currentUser = craft()->userSession->user;
+        if ($currentUser)
+        {
+            $data['uniqueid'] = $currentUser->id;
 
-		//$result = ob_get_contents(); // Store buffer in variable
-		
-		//ob_end_clean();
-		
-		return $result;
+            if (craft()->config->get('useEmailAsUsername'))
+                $data['name'] = $currentUser->getFullName();
+            else
+                $data['name'] = $currentUser->username;
+            $data['email'] = $currentUser->email;
+            if ($currentUser->getPhotoUrl())
+                $data['photourl'] = $currentUser->getPhotoUrl();
+        }
+
+        $vanillaforumsClientID = $settings['vanillaforumsClientID'];
+        $vanillaforumsSecret = $settings['vanillaforumsSecret'];
+
+        $secure = true;
+
+        //ob_start(); // Start output buffering
+
+        \WriteJsConnect($data, $_GET, $vanillaforumsClientID, $vanillaforumsSecret, $secure);
+
+        //$result = ob_get_contents(); // Store buffer in variable
+
+        //ob_end_clean();
+
+        return $result;
     } /* -- outputSSO */
 
 /* --------------------------------------------------------------------------------
-	Output the Vanillaforums SSO Embed string
+    Output the Vanillaforums SSO Embed string
 -------------------------------------------------------------------------------- */
 
     public function outputSSOEmbed($userId = 0)
     {
-		$result = "";
-		$settings = craft()->plugins->getPlugin('vanillaforums')->getSettings();
-		$data = array();
-		
-		$currentUser = craft()->userSession->user;
-		if ($currentUser)
-		{
-			$data['uniqueid'] = $currentUser->id;
-			$data['name'] = $currentUser->getFullName();
-			$data['email'] = $currentUser->email;
-			if ($currentUser->getPhotoUrl())
-				$data['photourl'] = $currentUser->getPhotoUrl();
-		}
-		
-		$vanillaforumsClientID = $settings['vanillaforumsClientID'];
-		$vanillaforumsSecret = $settings['vanillaforumsSecret'];
+        $result = "";
+        $settings = craft()->plugins->getPlugin('vanillaforums')->getSettings();
+        $data = array();
 
-		$secure = true;
-				
-		$result = \JsSSOString($data, $vanillaforumsClientID, $vanillaforumsSecret);
-		
-		return $result;
+        $currentUser = craft()->userSession->user;
+        if ($currentUser)
+        {
+            $data['uniqueid'] = $currentUser->id;
+            $data['name'] = $currentUser->getFullName();
+            $data['email'] = $currentUser->email;
+            if ($currentUser->getPhotoUrl())
+                $data['photourl'] = $currentUser->getPhotoUrl();
+        }
+
+        $vanillaforumsClientID = $settings['vanillaforumsClientID'];
+        $vanillaforumsSecret = $settings['vanillaforumsSecret'];
+
+        $secure = true;
+
+        $result = \JsSSOString($data, $vanillaforumsClientID, $vanillaforumsSecret);
+
+        return $result;
     } /* -- outputSSOEmbed */
 
 
